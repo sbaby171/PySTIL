02e17636f75e52a632cbe12f75e3b32b14229e6b
Max Sbabo <maximillian.sbabo@gmail.com>
2020-04-24 19:19:42 -0700

    Complete test for header_1.stil

--- tests/unit/test_header_parsing.py
+++ tests/unit/test_header_parsing.py
@@ -1,8 +1,31 @@
-import unittest
+import unittest, os 
 
+from PySTIL import stil
+
+
+PATH = os.path.join(os.getcwd(), "tests", "samples", "headers")
+DEBUG = 0
 class TestHeaderParsingMethods(unittest.TestCase):
     def test_header_1(self):
-        print("TODO: header_1")
+        _file = os.path.join(PATH, "header_1.stil")
+
+    
+
+        so = stil.STIL(sfp=_file, debug=DEBUG)
+        so.parse()
+
+        self.assertTrue(so.header.get_title() == "Sample STIL Title")
+        self.assertTrue(so.header.get_date() == "Tue Apr 28 12:23:48 EST 1996")
+        self.assertTrue(so.header.get_source() == "VHDL simulation on April 22, 1996")
+        for i, ann in enumerate(so.header.get_history()): 
+            if i == 0: self.assertTrue(ann == "rev1 - 4/21/96 - made some change")
+            if i == 1: self.assertTrue(ann == "rev2- 4/22/96 - made it work")
+
+
+
+        
+
+
         self.assertTrue(True)
     def test_header_2(self):
         print("TODO: header_2")
