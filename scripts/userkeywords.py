e8475f9e3c5a350510200c60419839d0e82e572a
Max Sbabo <maximillian.sbabo@gmail.com>
2020-07-06 15:46:09 -0700

    Updated

--- scripts/userkeywords.py
+++ scripts/userkeywords.py
@@ -1,28 +1,25 @@
 import os, sys, argparse
 import PySTIL.stil as pystil
 
-
-
 def _handle_cmd_args(): 
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", help="Increase console logging", action="store_true")
     parser.add_argument("--all", help="Tokenize entire STIL", action="store_true")
     parser.add_argument("stil", help="STIL file path",)
     args = parser.parse_args()
-   
     if not os.path.isfile(args.stil): 
         raise ValueError("Invalid STIL file.")
-
     return args
 
 if __name__ == "__main__":
     args = _handle_cmd_args()
+    # Standard Parsing
     stil = pystil.STIL(file = args.stil, debug = args.debug)
     stil.parse()
-
-
+    # Query parts
     userkeyword = "CustomExpansion"
     instances = stil.get_userkeyword("CustomExpansion")
+    print(instances)
     for instance in instances: 
         tokens = pystil.sutils.lex(instance)
         #print(tokens)
