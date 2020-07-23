d67b2dc9e9e9e39abae2177f9979662e382d7838
Max Sbabo <maximillian.sbabo@gmail.com>
2020-07-22 18:20:09 -0700

    Updated unit test for align with other test in terms of imports

--- tests/unit/test_userkeywords.py
+++ tests/unit/test_userkeywords.py
@@ -1,40 +1,35 @@
 import unittest, os 
-
-from PySTIL import stil
-
-#import PySTIL.stil as pystil
-
+import PySTIL.stil as pystil
 
 PATH = os.path.join(os.getcwd(), "tests", "samples", "userkeywords")
 DEBUG = 0
 class TestUserKeywords(unittest.TestCase):
     def test_userkeywords_1(self):
-        # NOTE: The names of the test should match with the name of the 
-        # stil file used. 
         filename = "userkeywords_1.stil"
-        so = stil.STIL(file=os.path.join(PATH, filename), debug=DEBUG)
-        so.parse()
-        failed = False
-        msg = []
+        # Standard parsing: 
+        stil = pystil.STIL(file=os.path.join(PATH, filename), debug=DEBUG)
+        stil.parse()
+        # Checks:  
+        failed = False; msg = []
         # TODO: condense all tplp entries intoa single list. 
         #    tplp[stil.1] -> {tpl : {start,end,etc.} }
         #    tplp[stil.2] -> {tpl : {start,end,etc.} }
         #    tplp[stil.3] -> {tpl : {start,end,etc.} }
         # 
         # [tpl1s, tpl2s, tpl ]
-        if 'diepad' not in so._tplp[filename]: 
+        if 'diepad' not in stil._tplp[filename]: 
             failed = True
             msg.append("TPLP is missing 'diepad' user keyword.")
-        if 'UserKeywords' not in so._tplp[filename]: 
+        if 'UserKeywords' not in stil._tplp[filename]: 
             failed = True
             msg.append("TPLP is missing 'UserKeywords' keyword.")
-        if 'UserFunctions' not in so._tplp[filename]: 
+        if 'UserFunctions' not in stil._tplp[filename]: 
             failed = True
             msg.append("TPLP is missing 'UserFunctions' keyword.")
-        if 'tchn' not in so._tplp[filename]: 
+        if 'tchn' not in stil._tplp[filename]: 
             failed = True
             msg.append("TPLP is missing 'tchn' user keyword.")
-        if 'abs_min' not in so._tplp[filename]: 
+        if 'abs_min' not in stil._tplp[filename]: 
             failed = True
             msg.append("TPLP is missing 'abs_min' user keyword.")
 
