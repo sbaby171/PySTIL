91c24167665a31ffa4ae37f66c8373bdb9286f53
Max Sbabo <maximillian.sbabo@gmail.com>
2020-08-05 21:42:33 -0700

    Added ordering to internal PatternBurst blocks

--- PySTIL/PatternBurst.py
+++ PySTIL/PatternBurst.py
@@ -25,35 +25,29 @@ class PatternBurst(object):
         # TODO: Error if empty?  
         self.name = name 
 
-        self.patLists = []
-        self.patSets  = []
-        self.parallelPatLists = []
+        #self.patLists = []
+        #self.patSets  = []
+        #self.parallelPatLists = []
+
+        self.blocks = {"PatList": [], "ParallelPatList":[], "PatSet":[]}
+        self.ordering = [] # [(type,index)]
+
+        # TODO: Internal blocks of the PatternBurst need to be 
+        # kept in order. 
     
     def add(self, entity): 
         if isinstance(entity, PatList): 
-            self.parallelPatLists.append(entity)
+            self.blocks["PatList"].append(entity)
+            self.ordering.append(("PatList",len(self.blocks["PatList"])-1))
         elif isinstance(entity, ParallelPatList): 
-            self.patLists.append(entity)
+            self.blocks["ParallelPatlist"].append(entity)
+            self.ordering.append(("ParallelPatList",len(self.blocks["ParallelPatList"])-1))
         elif isinstance(entity, PatSet): 
-            self.patSets.append(entity)
+            self.blocks["PatSet"].append(entity)
+            self.ordering.append(("PatSet",len(self.blocks["PatSet"])-1))
         else: 
             raise RuntimeError("Must provide instance of either "\
                 "PatList, ParallelPatList, or PatSet.")
-        
-
-
-
-    #def add(self, entity): 
-    #    if isinstance(entity, Category): 
-    #        if entity.name in self.categories: 
-    #            raise RuntimeError("Category (%s) is already defined."%(entity.name))
-    #        else: self.categories[entity.name] = entity
-    #    elif isinstance(entity, Variable): 
-    #        if entity.name in self.variables: 
-    #            raise RuntimeError("Variable (%s) is already defined."%(entity.name))
-    #        else: self.variables[entity.name] = entity
-    #    else: 
-    #        raise ValueError("Entity is of to be classes 'Category' or 'Variable'.")
 
 def create_PatternBurst(string, name = "", file = "", debug=False):
     """ 
@@ -67,69 +61,66 @@ def create_PatternBurst(string, name = "", file = "", debug=False):
     if debug: 
         print("DEBUG: (%s): Tokens: %s "% (func, tokens))
         print("DEBUG: (%s): SymbolTable: %s"%(func, sytbl))
+    
+    # TODO: Keep order of the PatList, ParallelPatList, PatSet
+    tmap = {} 
+    for i in sytbl["PatList"]: tmap[i] = "PatList"
+    for i in sytbl["ParallelPatList"]: tmap[i] = "ParallelPatList"
+    for i in sytbl["PatSet"]: tmap[i] = "PatSet"
 
-    if "ParallelPatList" in sytbl:
-        print("Contains ParrallelPatList") 
-
-    # ParallelPatList: 
-    for i in sytbl["ParallelPatList"]: 
-        cbs, cbe = sytbl.get_next_curly_set(i)
-        ppl = ParallelPatList()
-
-        # Handle ParallelPatList Mode.
-        if cbs - i == 2: 
-            if tokens[i+1]['tag'] not in ParallelPatList.modes: 
-                raise RuntimeError("ParallelPatList must be of modes"\
+    todos = []
+    [todos.append((key,value)) for (key, value) in sorted(tmap.items())]
+    for todo in todos: 
+        i = todo[0]; block = todo[1]
+        if block == "ParallelPatList": 
+            cbs, cbe = sytbl.get_next_curly_set(i)
+            ppl = ParallelPatList()
+            # Handle ParallelPatList Mode.
+            if cbs - i == 2: 
+                if tokens[i+1]['tag'] not in ParallelPatList.modes: 
+                    raise RuntimeError("ParallelPatList must be of modes"\
                     "%s. Recieved the following: %s"%(ParallelPatList.modes, tokens[i+1]['token']))
-            else: ppl.mode = tokens[i+1]['tag']
-        print(ppl.mode)
+                else: ppl.mode = tokens[i+1]['tag']
+            print(ppl.mode)
 
-        j = cbs + 1; jend = cbe - 1 
+            j = cbs + 1; jend = cbe - 1 
+            # State flags: 
+            inPattern = False
+            inSignalGroups = False 
+            inMacroDefs = False 
+            inProcedures = False 
+            inScanStructures = False 
+            inStart = False; inStop = False
+            inTermination = False 
 
+            pat = ""; cbcount = 0 
+            while j <= jend: 
+                token = tokens[j]['token']
+                tag   = tokens[j]['tag'] 
+                # State dependent: 
+                if inPattern: 
+                    if tag == "}": cbcount -= 1
+                    if cbcount == 0: inPattern = 0; j+=1; continue 
+                    if tag == "Extend": 
+                        ppl[pat]["Extend"] = True
+                        if tokens[j+1]['tag'] != ';': 
+                            raise RuntimeError("Expecting ';' after Extend statment.")
+                        # TODO: if tag == "SignalGroups", etc.
+                    j+=1; continue 
 
-        # State flags: 
-        inPattern = False
-        inSignalGroups = False 
-        inMacroDefs = False 
-        inProcedures = False 
-        inScanStructures = False 
-        inStart = False; inStop = False
-        inTermination = False 
-
-        pat = ""
-        cbcount = 0 
-        while j <= jend: 
-            token = tokens[j]['token']
-            tag   = tokens[j]['tag'] 
-            
-            # State dependent: 
-            if inPattern: 
-                if tag == "}": cbcount -= 1
-                if cbcount == 0: 
-                    inPattern = 0; j+=1; continue 
-                if tag == "Extend": 
-                    ppl[pat]["Extend"] = True
-                    #ppl.extend(pat)
-                    if tokens[j+1]['tag'] != ';': 
-                        raise RuntimeError("Expecting ';' after Extend statment.")
-
-                j+=1; continue 
-
-            # Freelance: 
-            if tag == 'identifier': 
-                pat = token; ppl.add(token)
-                if tokens[j+1]['tag'] == ";": 
-                    j += 2; continue 
-                elif tokens[j+1]['tag'] == "{":
-                    cbcount = 1 
-                    inPattern = True
-                    j += 2; continue 
-                else: raise RuntimeError("Expecting Pattern name.")
-            j += 1
-        print(ppl.patterns)
-        pb.add(ppl)    
-        
-
+                # Freelance: 
+                if tag == 'identifier': 
+                    pat = token; ppl.add(token)
+                    if tokens[j+1]['tag'] == ";": 
+                        j += 2; continue 
+                    elif tokens[j+1]['tag'] == "{":
+                        cbcount = 1 
+                        inPattern = True
+                        j += 2; continue 
+                    else: raise RuntimeError("Expecting Pattern name.")
+                j += 1
+            print(ppl.patterns)
+            pb.add(ppl)    
     return pb
 
 
