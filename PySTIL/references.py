815d65b3eb080a6b5e60cbc6f58c7334be0ebb7f
Max Sbabo <maximillian.sbabo@gmail.com>
2022-08-03 19:32:09 -0700

    Create references.py
    References file will be used during parsing. Its mainly meant to carry reserved keywords and characters.

---
+++ PySTIL/references.py
@@ -0,0 +1,28 @@
+GLOBAL_DOMAIN = "__GLOBAL__"
+
+TOP_LEVEL_KEYWORDS = [
+  "STIL", 
+  "Include", 
+  "Header", 
+  #"Ann" # Ignoring for now becaue it would be caught w/in other blocks!
+  "Signals", 
+  "SignalGroups",
+  "ScanStructures", 
+  "Spec", 
+  "DCLevels",
+  "DCSets", 
+  "Timing", 
+  "Selector", 
+  "PatternBurst", 
+  "PatternExec", 
+  "Procedures", 
+  "MacroDefs",
+  "Environment", 
+  "PatternFailReport", 
+  "Pragma", 
+  "UserKeywords",
+  "UserFunctions",
+  "Pattern"
+]
+OTHER_KEYWORDS = [
+]
