c3c7f9819b36ff73c6c25bb81f702ecdabffe485
Max Sbabo <maximillian.sbabo@gmail.com>
2022-08-04 14:44:14 -0700

    Update stil.py
    Added clean-up block after first pass to remove all references to top-level blocks, within blocks. The location for this distinction is at he location of the PatternExec block.

--- PySTIL/stil.py
+++ PySTIL/stil.py
@@ -18,6 +18,9 @@ def read(stilfile,debug=False):
     TLBs = list(funcs.references.TOP_LEVEL_KEYWORDS)
     # ========================================================================:
     # (1) First Pass 
+    # The first pass makes the assumption the the TOP-LEVEL block with start
+    # on its own line; in that it will be the first identifier on a line. 
+    # 
     # On first pass, we should becareful to only grab TOP-level blocks. This 
     # emphasis is becasue some top-level blocks can also be found within other
     # blocks. For example, the "Ann" block can be found within other blocks. 
@@ -36,12 +39,12 @@ def read(stilfile,debug=False):
         if not line: continue 
         if line.startswith("//"): continue 
         for kw in TLBs: # Moved out refs.TOP_LEVEL for optimzations
-            if re.search("^%s"%(kw),line): 
+            if re.match("^%s\s|$|{"%(kw),line): 
                 domain = funcs.get_domain_name(line,kw)
                 if kw not in toplevelblocks: toplevelblocks[kw] = []
                 toplevelblocks[kw].append({"line-count":ln, "source-file":1,"domain":domain})
                 toplevellines.append(int(ln))
-                if kw == "Pattern": TLBs = ["Pattern"]
+                if kw == "Pattern": TLBs = set(["Pattern"])
                 break
     pass1End = time.time()
     funcs.report_filesize_and_processing(pass1Start,pass1End,ln,stilfile,debug=debug)
@@ -56,6 +59,22 @@ def read(stilfile,debug=False):
     # NOTE: at this point, we have grabbed all the top-level tags, However, 
     # there will be false top-level tags due to references within blocks to 
     # other blocks. In some stils, I see 
+    if len(toplevelblocks["PatternExec"]) > 1: 
+        raise RuntimeError("Found more than 1 PatternExec block")
+    lnPatternExec = toplevelblocks["PatternExec"][0]["line-count"]
+    for kw, instances in toplevelblocks.items(): 
+        if instances.__len__() > 1: 
+            print("Multiple instances of %s"%(kw))
+            for i,instance in enumerate(instances): 
+                if instance["line-count"] > lnPatternExec: 
+                    print("Not top-level instance: %s"%(instance))
+                    del toplevelblocks[kw][i]
+    for kw,instances in toplevelblocks.items(): 
+        print(kw + ": ")
+        for instance in instances: 
+            print("- %s"%(instance))
+    
+    print("DEBUG: All first pass activities done")
     # ========================================================================:
     # (2) Second pass: 
     toplevelblocks["Signals"]
