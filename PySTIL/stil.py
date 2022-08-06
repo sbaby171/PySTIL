3699fb265b6198ad20e77bc7189d5a795d347b02
Max Sbabo <maximillian.sbabo@gmail.com>
2022-08-05 17:43:09 -0700

    Update stil.py
    Added SignalGroups class and builder function. Simply storing the raw expression. Next update is to evaluate the expression

--- PySTIL/stil.py
+++ PySTIL/stil.py
@@ -5,6 +5,24 @@ import re
 from collections import OrderedDict
 import funcs
 # ============================================================================:
+# Stages of Compiler: 
+# -PySTIL/compiler.py::PySTIL.compiler.read() 
+# TLBs = Top-Level-Blocks
+# 
+# NOTES: by default we ignore the ScanStructures. We will locate their 
+# starting lines but will NEVER parse unless explictily asked.
+#
+# NOTE: There is an optimization f
+#
+# 1.) Pre-processing: Locate line-numbers for all TLBs
+#     - Locate all the PatternExec blocks. If multiple, user must indicate 
+#       which one to use? If not defined, we defatul to the GLOBAL-DOMAIN. 
+#     - From this PatternExec block we pull all pieces (expect patterns)
+#       al parse them out. 
+#     - NOTE: There is an optimzation to skip skip all Pattern searching after
+#       the first one is found. This is going to be set as default
+# 
+# ============================================================================:
 class STIL(object): 
     def __init__(self,*args,**kwargs): 
         pass 
@@ -34,7 +52,6 @@ def build_Signals(tokens):
     SG = Signals(domain)
     i = fi; end = len(tokens)-1; cbs = 1; 
     while i <= end: 
-        print(i)
         # EXIT----------------------------------------------------------------:
         if tokens[i]["token"] == "}": 
             cbs -= 1; 
@@ -46,9 +63,8 @@ def build_Signals(tokens):
                 if tokens[i+1]['tag'] in typelist: 
                     sname = tokens[i]["token"]
                     stype = tokens[i+1]["tag"]
-                    print("DEBUG: [%s]: Adding Signal '%s'"%(func,sname))
-                    #SG.add(sname) # TODO: Signal Type 
-                    SG[sname] = {"type":stype} # TODO: Type 
+                    #print("DEBUG: [%s]: Adding Signal '%s'"%(func,sname))
+                    SG[sname] = {"type":stype}  
                     i += 3; continue 
                 else: raise RuntimeError("Bad")
             elif tokens[i+2]["tag"] == "{":
@@ -57,6 +73,58 @@ def build_Signals(tokens):
         # --------------------------------------------------------------------:
         i+=1 
     return SG
+# ============================================================================:
+class SignalGroups(object): 
+    def __init__(self,name,): 
+        self.name = name 
+        self._dict = OrderedDict()
+        self._expr = OrderedDict() # Holds raw expression for read
+
+    def add_expr(self,name,expression):
+        self._expr[name] = expression 
+# ============================================================================:
+def build_SignalGroups(tokens): 
+    func = "build_SignalGroups"
+    domain = funcs.references.GLOBAL_DOMAIN
+    if tokens[0]["token"] != "SignalGroups": 
+        raise RuntimeError("First SignalGroups token is not 'SignalGroups'")
+    fi = -1 
+    if tokens[1]["tag"] == "identifier": 
+        domain = tokens[1]["token"]
+        if tokens[2]["token"] != "{": 
+            raise RuntimeError("First SignalGroups token is not 'SignalGroups'")
+        fi = 3
+    elif tokens[1]["tag"] == "{": fi = 2 
+    else: raise RuntimeError("Issue with SignalGroups tokens: %s"%(tokens))
+
+    sg = SignalGroups(domain)
+    if fi == -1: raise RuntimeError("fi is off")
+    i = fi; end = len(tokens)-1; cbs = 1; 
+    while i <= end: 
+        # EXIT----------------------------------------------------------------:
+        if tokens[i]["token"] == "}": 
+            cbs -= 1; 
+            if cbs == 0: 
+                break 
+        # --------------------------------------------------------------------:
+        if tokens[i]['tag'] == "identifier": 
+            if tokens[i+1]['tag'] != "=": 
+                print("ERROR:     i = %s, %s"%(i,tokens[i]))
+                print("ERROR: 1 + i = %s, %s"%(i+1,tokens[i+1]))
+                raise RuntimeError("need '=' after identifier")
+            j = i 
+            while j < end:
+                if tokens[j]['tag'] == ";": 
+                    sname = tokens[i]["token"]
+                    expression = "".join([tokens[k]["token"] for k in range(i+2,j+1)])
+                    print("DEBUG: [%s]: Adding: %s = %s"%(func,sname,expression))
+                    sg.add_expr(sname,expression)
+                    i = j; break
+                j += 1
+                if j == end:raise RuntimeError("bad")
+        # --------------------------------------------------------------------:
+        i += 1
+    return sg 
 # ============================================================================:
 class PatternExec(object): 
     def __init__(self,name,timing="",patternBurst=""): 
@@ -104,7 +172,7 @@ def build_PatternExec(tokens):
         i += 1
     return PE
 # ============================================================================:
-def read(sf,debug=False): 
+def read(sf,allow_fullbreak=True,debug=False): 
     func = "PySTIL.stil.read"
     if not os.path.isfile(sf): 
         raise ValueError("Provide STIL file doesn't exist or has restrictive"\
@@ -130,16 +198,23 @@ def read(sf,debug=False):
     tlls = [] # To hold all starting line keys
     stilIndex = {1:sf} # NOTE: Include files will add to this. 
     pass1Start = time.time()
+    fullbreak = False 
     for ln,line in funcs._read_line(sf): 
         if not line: continue 
-        if line.startswith("//"): continue 
+        if line.startswith("//"): continue
+        if fullbreak: 
+            if allow_fullbreak: ln = -1; break;
+            else: fullbreak = False 
         for kw in TLBs: # Moved out refs.TOP_LEVEL for optimzations
             if re.match("^%s\s|$|{"%(kw),line): 
                 domain = funcs.get_domain_name(line,kw)
                 if kw not in tlbs: tlbs[kw] = []
                 tlbs[kw].append({"line-count":ln, "source-file":1,"domain":domain})
                 tlls.append(int(ln))
-                if kw == "Pattern": TLBs = set(["Pattern"])
+                #if kw == "Pattern": TLBs = set(["Pattern"]) # NOTE
+                if kw == "Pattern": 
+                    if allow_fullbreak: fullbreak = True
+                    else: TLBs = set(["Pattern"])
                 break
     pass1End = time.time()
     funcs.report_filesize_and_processing(pass1Start,pass1End,ln,sf,debug=debug)
@@ -183,27 +258,31 @@ def read(sf,debug=False):
     lnEnd = tlls[tlls.index(lnPatternExec) + 1] - 1
     pxlines = [line for ln,line in funcs._read_line_range(sf,lnPatternExec, lnEnd)]
     pxtokens = funcs.lex(pxlines)
-    pe = build_PatternExec(pxtokens)
-    print(pe)
-    print(pe.name)
-    print(pe.timing)
-    print(pe.patternBurst)
-
-
+    patternExec = build_PatternExec(pxtokens)
+    print(patternExec)
+    print(patternExec.name)
+    print(patternExec.timing)
+    print(patternExec.patternBurst)
+    # -----------------------------------------------------------------------:
+    # Parse the Signals block
     lnS = tlbs["Signals"][0]["line-count"]
     lnE   = tlls[tlls.index(lnS) + 1] - 1
     sglines = [line for ln,line in funcs._read_line_range(sf,lnS,lnE)]
     sgtokens = funcs.lex(sglines)
-    for tk in sgtokens: 
-        print(tk)
-    sg = build_Signals(sgtokens)
-    print(sg)
+    signals = build_Signals(sgtokens)
+    #print(signals)
+    print(signals.name)
+    print(signals.__len__())
+    # -----------------------------------------------------------------------:
+    # Parse the SignalGroups
+    lnS = tlbs["SignalGroups"][0]["line-count"]
+    lnE   = tlls[tlls.index(lnS) + 1] - 1
+    sglines = [line for ln,line in funcs._read_line_range(sf,lnS,lnE)]
+    sgtokens = funcs.lex(sglines)
+    for tk in sgtokens: print(tk)
+    sg = build_SignalGroups(sgtokens)
     print(sg.name)
-    print(sg.__len__())
-
-
-
- 
+    for n,e in sg._expr.items(): print(n,e)
     print("DEBUG: All first pass activities done")
     # ========================================================================:
     # (2) Second pass: 
